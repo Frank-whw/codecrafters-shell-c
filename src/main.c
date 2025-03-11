7cd9d4c5db53c151c9d094494a4c886a31cf5af0
Frank-whw <2120320012@qq.com>
2025-03-11 19:58:07 +0800

    [any message]

--- src/main.c
+++ src/main.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 
     fgets(input, 100, stdin);
     input[strlen(input) - 1] = '\0';
-    printf("%s: command not found", input);
+    printf("%s: command not found\n", input);
   }
 
   return 0;
