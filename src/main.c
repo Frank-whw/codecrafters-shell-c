d736fd29cdc105d938164149651e3945d462d6c5
Frank-whw <2120320012@qq.com>
2025-03-11 19:57:29 +0800

    [any message]

--- src/main.c
+++ src/main.c
@@ -6,14 +6,14 @@ int main(int argc, char *argv[])
 {
   // Flush after every printf
   setbuf(stdout, NULL);
-
-  // Uncomment this block to pass the first stage
-  printf("$ ");
-
-  // Wait for user input
-  char input[100];
   while (true)
   {
+    // Uncomment this block to pass the first stage
+    printf("$ ");
+
+    // Wait for user input
+    char input[100];
+
     fgets(input, 100, stdin);
     input[strlen(input) - 1] = '\0';
     printf("%s: command not found", input);
