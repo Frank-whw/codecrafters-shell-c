3935d8d761355d0227ee72a4000cfb101e201e0b
Frank-whw <2120320012@qq.com>
2025-03-11 22:08:52 +0800

    [any message]

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <unistd.h>
 // void substring(char *src, char *dest, int start, int len)
 // {
 //   int i;
@@ -11,6 +12,37 @@
 //   }
 //   dest[i] = '\0';
 // }
+
+// Function to check if a file exists and is executable
+bool isExecutable(char *path)
+{
+  return access(path, X_OK) == 0;
+}
+// Function to search for a command in the PATH
+char *findPath(char *command)
+{
+  char *path_env = getenv("PATH");
+  if (path_env == NULL)
+  {
+    return NULL;
+  }
+  char *path_copy = strdup(path_env);
+  char *dir = strtok(path_copy, ":");
+  static char full_path[1024];
+  while (dir != NULL)
+  {
+    snprintf(full_path, sizeof(full_path), "%s/%s", dir, command);
+    if (is_executable(full_path))
+    {
+      free(path_copy);
+      return full_path;
+    }
+    dir = strtok(NULL, ":");
+  }
+  free(path_copy);
+  return NULL;
+}
+
 int main(int argc, char *argv[])
 {
   // Flush after every printf
@@ -22,7 +54,7 @@ int main(int argc, char *argv[])
 
     // Wait for user input
     char input[100];
-
+    char builtins[][16] = {"echo", "type", "exit"};
     fgets(input, 100, stdin);
     input[strlen(input) - 1] = '\0';
     if (strcmp(input, "exit 0") == 0)
@@ -38,14 +70,30 @@ int main(int argc, char *argv[])
     }
     else if (strncmp(input, "type", 4) == 0)
     {
-      if (strncmp(input + 5, "type", 4) == 0 || strncmp(input + 5, "echo", 4) == 0 || strncmp(input + 5, "exit", 4) == 0)
+      int hasFount = 0;
+      for (int i = 0; i < sizeof(builtins) / 16; i++)
       {
-        printf("%s is a shell builtin\n", input + 5);
+        if (strcmp(input + 5, builtins[i]) == 0)
+        {
+          hasFount = 1;
+          printf("%s is a shell builtin\n", input + 5);
+        }
       }
-      else
+      if (hasFount == 0)
       {
+        char *path = findPath(input + 5);
+        if (path)
+        {
+          hasFount = 1;
+          printf("%s is %s\n", input + 5, path);
+        }
+        else
+        {
+          printf("%s: not found\n", input + 5);
+        }
+      }
+      if (hasFount == 0)
         printf("%s: not found\n", input + 5);
-      }
     }
     else
       printf("%s: command not found\n", input);
